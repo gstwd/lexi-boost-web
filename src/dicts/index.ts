a786855029bcd1a76309d70b6e16aaa9251a3ea5
zhangfeng <zhangfeng@cqczxy.com>
2025-10-08 20:57:19 +0800

    project architecture optimization

--- src/dicts/index.ts
+++ src/dicts/index.ts
@@ -1,16 +1,16 @@
-import { DictOption, DictsType } from "@/utils/dict";
+import { DictOption, DictsType } from '@/utils/dict'
 
 // 录入单词的位置枚举
 
 const WordEntryLocationDict: DictOption[] = [
-    { value: 0, label: '未知' },
-    { value: 1, label: '家' },
-    { value: 2, label: '公司' },
-    { value: 3, label: '旅行' },
-    { value: 4, label: '其他' }
+  { value: 0, label: '未知' },
+  { value: 1, label: '家' },
+  { value: 2, label: '公司' },
+  { value: 3, label: '旅行' },
+  { value: 4, label: '其他' }
 ]
 
 // 统一创建字典
 export const Dicts: DictsType = {
-  'wordEntryLocation': WordEntryLocationDict,
+  wordEntryLocation: WordEntryLocationDict
 }
